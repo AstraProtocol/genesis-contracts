d30a7f1727f630acb5013d0df01728fc0f7a7f7b
lucabada <luan.do1@tiki.vn>
2022-07-04 15:41:25 +0700

    init genesis contracts

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# genesis-contracts
 This repo will contain all contracts which are pre-deployed at genesis.json
---
+++ contracts/Reward-Provider.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.6.4;
+
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+    @title Facilitates registration of contract to get metadata for migration
+ */
+contract RewardProvider is Ownable, Pausable{
+    event SetAdmin(address indexed addr, bool isEnabled);
+    event Transfer(address indexed addr, uint amount);
+
+    constructor(address _owner) public{
+        _transferOwnership(_owner);
+    }
+
+    function transfer(address receiver, uint amount) external whenNotPaused onlyOwner(){
+        payable(receiver).transfer(amount);
+        emit Transfer(receiver, amount);
+    }
+}
